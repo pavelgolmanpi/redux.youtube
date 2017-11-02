68277611aeab8d611b245e666a187ef8021a57fa
Pavel Golman <pavel.golman@postindustria.com>
2017-11-02 15:21:37 +0200

    Init project. Created Search Filter

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchInputFilter from '../components/SearchInputFilter.js'
+
+const App = () => (
+  <div>
+    <h2>YouTube API</h2>
+    <hr/>
+    <SearchInputFilter />
+  </div>
+)
+
+export default App
